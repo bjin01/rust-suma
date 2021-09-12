562410ff03878685ad4a4ee37ef3581f27b58098
bjin01 <jinbo01@hotmail.com>
2021-09-12 17:52:04 +0200

    yaml func

--- src/test.rs
+++ src/test.rs
@@ -1,9 +1,19 @@
 extern crate xmlrpc;
-
+extern crate chrono;
+//use std::{any::type_name};
 use xmlrpc::{Request, Value};
+use clap::{Arg, App};
 use serde::{Serialize, Deserialize};
 use std::io::prelude::*;
 use std::fs::File;
+/* #[allow(dead_code)]
+fn type_of<T>(_: T) -> &'static str {
+    type_name::<T>()
+} */
+
+// static SUMAHOST: &'static str = "http://bjsuma.bo2go.home/rpc/api";
+// static USER: &str = "bjin";
+// static PWD: &str = "suse1234";
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
 struct SumaInfo {
@@ -24,9 +34,12 @@ impl SumaInfo {
         let deserialized_map: SumaInfo = match serde_yaml::from_str(&buffer) {
             Ok(i) => i,
             Err(_) => panic!("getting yaml failed.")
+            
         };
+        //println!("{:?}", &deserialized_map);
         return deserialized_map
     }
+
 }
 
 fn login(s: &SumaInfo) -> String {
@@ -60,8 +73,28 @@ fn logout(k: &String, s: &SumaInfo) -> i32 {
     }
 }
 
+/* fn get_id(x: Value) -> i32 {
+    match x {
+        Value::Int(i) => {
+            return i
+        }
+        Value::Int64(_) => todo!(),
+        Value::Bool(_) => todo!(),
+        Value::String(_) => todo!(),
+        Value::Double(_) => todo!(),
+        Value::DateTime(_) => todo!(),
+        Value::Base64(_) => todo!(),
+        Value::Struct(_) => todo!(),
+        Value::Array(_) => todo!(),
+        Value::Nil => todo!(),
+        
+    }
+} */
+
+
 
 fn printvalue(x: &Value, s: &Vec<String>, id_list: &mut Vec<i32>) {
+    //let mut id_list: Vec<i32> = Vec::new();
     match x {
         Value::Int(i) => {
             println!("{}", i);
@@ -143,8 +176,52 @@ fn main() -> Result<(), serde_yaml::Error> {
     suma_info.hostname.push_str("/rpc/api");
     println!("suma host api url: {:?}", &suma_info.hostname);
 
+    let matches = App::new("SUMA Get Something")
+        .version("1.0")
+        .author("Bo Jin <bo.jin@suse.com>")
+        .about("make suma api calls")
+        .arg(Arg::new("hostname")
+            .short('h')
+            .long("hostname")
+            .about("SUMA host name or ip")
+            .takes_value(true))
+        .arg(Arg::new("user")
+            .short('u')
+            .long("username")
+            .about("SUMA api user name")
+            .takes_value(true))
+        .arg(Arg::new("passwd")
+            .short('p')
+            .long("password")
+            .about("SUMA user password")
+            .takes_value(true))
+        .get_matches();
+
+    if let Some(i) = matches.value_of("hostname") {
+        println!("Value for hostname: {}", i.to_string());
+    }
+
+    if let Some(i) = matches.value_of("user") {
+        println!("Value for user: {}", i);
+    }
+
+    if let Some(i) = matches.value_of("passwd") {
+        println!("Value for password: {}", i);
+    }
+
     
     let mut server_id_list: Vec<i32> = Vec::new();
+    /* let search: Vec<String> = vec!["label".to_string(), "arch_name".to_string(), "packages".to_string()];
+    let key = login().unwrap(); 
+
+    let channellist = Request::new("channel.listAllChannels").arg(String::from(&key));
+    let channellist_result = channellist.call_url(SUMAHOST);
+    printvalue(&channellist_result.unwrap(), &search); */
+    //println!("channel list: {:?}", &channellist_result.unwrap());
+
+    //let search: Vec<String> = vec!["id".to_string(), "advisory_synopsis".to_string(), "advisory_name".to_string(), "update_date".to_string()];
+    // let search: Vec<String> = vec!["id".to_string()];
+    // let systems: Vec<String> = vec!["caasp01.bo2go.home".to_string(), "caasp02.bo2go.home".to_string()];
     let key = login(&suma_info);
 
     for s in &suma_info.servers {        
@@ -162,11 +239,11 @@ fn main() -> Result<(), serde_yaml::Error> {
     if server_id_list.len() > 0 {
         for i in &server_id_list {     
             let mut patch_id_list: Vec<i32> = Vec::new();
-            
+            println!("true or false suma_info.advisor_type.trim().is_empty() {}", &suma_info.advisory_type.trim().len());
             if suma_info.advisory_type.trim().len() <= 1 {
                 let erratalist = Request::new("system.getRelevantErrata").arg(String::from(&key)).arg(*i);
                 let erratalist_result = erratalist.call_url(String::from(&suma_info.hostname));
-                
+                println!("in if condition suma_info.advisor_type.trim().is_empty()");
                 match erratalist_result {
                     Ok(i) => printvalue(&i, &suma_info.output_fields, &mut patch_id_list),
                     Err(e) => println!("no errata found: {:?}", e),
@@ -193,9 +270,21 @@ fn main() -> Result<(), serde_yaml::Error> {
                 },
                 Err(e) => println!("no patch job created because the errata list is empty. Maybe your system is up to date.: {:?}", e),
             }
+            
         }
     }
+    /* let erratalist = Request::new("system.getRelevantErrataByType").arg(String::from(&key)).arg(1000010001).arg(String::from("Security Advisory"));
+    
+    let erratalist_result = erratalist.call_url(SUMAHOST);
+    
+    printvalue(&erratalist_result.unwrap(), &search, &mut id_list);
+    println!("id_list: {:?}", &id_list); */
+    
 
+
+    //let key = login().unwrap();
+    //println!("session key is: {}", &key);
+    //let logout_id = logout(&key);
     println!("Logout successful - {}", logout(&key, &suma_info));
     Ok(())
 }
